fc4075c6828ad447fce322cb39ceac538a1474b4
EzHawkeye <66911686+EzHawkeye@users.noreply.github.com>
2020-06-15 15:11:49 +0200

    Update serverip.js

--- commands/serverip.js
+++ commands/serverip.js
@@ -2,7 +2,7 @@ const discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
 
-    return message.channel.send("Westside IP: 95.217.239.248:30120");
+    return message.channel.send("Westside IP:   95.217.239.248:30120");
 
 }
 
